43107ee98139d55f1dbfa72967af63b684345dcc
Prince Chahar <princechahar00007gmail.com>
2026-08-02 13:08:14 +0530

    Python Codes

---
+++ PYTHON/day1.py
@@ -0,0 +1,10 @@
+# PYTHON DAY 1 
+
+# MY FRIST PYTHON PROGRAM 
+
+print("Hi")
+print("My name is Prince Chahar") 
+print("I am a BCA Student") 
+print("I am Learning Python ") 
+print("Welcome to Skill Mission 365") 
+
